32037ae84e4feb1bd45c6c7b374b6bc8ad9de172
ValeWenz <valentino.wenzlich@gmail.com>
2026-04-04 14:35:02 +0200

    eserc.1_lab(I)

--- esercitazione1_lab(I).py
+++ esercitazione1_lab(I).py
@@ -1,3 +1,4 @@
+"""
 class ExamException (Exception):
     pass
 class MovingAverage:
@@ -18,3 +19,35 @@ class MovingAverage:
 moving_average=MovingAverage(2)
 result=moving_average.compute([2,4,8,16])
 print(result)
+"""
+
+class ExamException(Exception):
+    pass
+
+class MovingAverage():
+    def __init__(self,window_length):
+        if(isinstance(window_length,int)):
+            if(window_length>0):
+                self.window_length=window_length
+            else:
+                raise ExamException("numero minore o uguale a 0")
+        else:
+            raise ExamException("la finestra deve essere un numero strettamente positivo")
+    def compute(self,data):
+        if(not isinstance(data,list)): raise ExamException("l'argomento non è una lista")
+        if(len(data)<self.window_length):
+            raise ExamException("finestra maggiore del numero di argomenti della lista")
+        for idx,elem in enumerate(data):
+            if(not isinstance(elem,int) or isinstance(elem,float)):
+                raise ExamException(f"la lista contiene un elemento che non è un numero alla posizione {idx}")
+        output=[]
+        for i in range(len(data)-self.window_length+1):
+            sum=0
+            for n in range(self.window_length):
+                sum=sum+data[i+n]
+            media=float(sum/self.window_length)
+            output.append(media)
+        return output
+    
+mv=MovingAverage(2)
+print(mv.compute([2,4,8,16]))
